51244214413b54844def34cd061671861935776c
Erick Aguilar <ericksvaguilar@gmail.com>
2021-02-14 08:30:45 -0300

    refactor utilities.js

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -1,8 +1,8 @@
-export const isValidForm = (form) => {
-  const nameError = document.querySelector('#name + small.form__error-message');
-  const emailError = document.querySelector('#email + small.form__error-message');
-  const phoneError = document.querySelector('#phone + small.form__error-message');
-  const phone2Error = document.querySelector('#phone2 + small.form__error-message');
+export const isValidForm = form => {
+  const nameErr = document.querySelector('#name + [data-js="error-msg"]');
+  const emailErr = document.querySelector('#email + [data-js="error-msg"]');
+  const phoneErr = document.querySelector('#phone + [data-js="error-msg"]');
+  const phone2Err = document.querySelector('#phone2 + [data-js="error-msg"]');
 
   let nameValidity = form.name.validity;
   let emailValidity = form.email.validity;
@@ -10,54 +10,77 @@ export const isValidForm = (form) => {
   let phone2Validity = form.phone2.validity;
 
   if (form.checkValidity()) {
-    nameError.textContent = '';
-    emailError.textContent = '';
-    phoneError.textContent = '';
-    phone2Error.textContent = '';
+    nameErr.textContent = '';
+    emailErr.textContent = '';
+    phoneErr.textContent = '';
+    phone2Err.textContent = '';
     return true;
   } else {
     // Validate name
     if (!nameValidity.valid) {
       if (nameValidity.valueMissing) {
-        nameError.textContent = 'Por favor, insira seu nome completo';
+        nameErr.textContent = 'Por favor, insira seu nome completo';
       } else if (nameValidity.patternMismatch) {
-        nameError.textContent = 'Nome inválido (apenas letras permitidas)';
+        nameErr.textContent = 'Nome inválido (apenas letras permitidas)';
       }
     } else {
-      nameError.textContent = '';
+      nameErr.textContent = '';
     }
 
     // Validate email
     if (!emailValidity.valid) {
       if (emailValidity.valueMissing) {
-        emailError.textContent = 'Por favor, insira seu email';
+        emailErr.textContent = 'Por favor, insira seu email';
       } else if (emailValidity.typeMismatch) {
-        emailError.textContent = 'Email inválido';
+        emailErr.textContent = 'Email inválido';
       }
     } else {
-      emailError.textContent = '';
+      emailErr.textContent = '';
     }
 
     // Validate Phone
     if (!phoneValidity.valid) {
       if (phoneValidity.valueMissing) {
-        phoneError.textContent = 'Por favor, insira seu telefone';
+        phoneErr.textContent = 'Por favor, insira seu telefone';
       } else if (phoneValidity.patternMismatch) {
-        phoneError.textContent = `Precisamos de um telefone no formato: (88) 98888-8888 ou (11) 8888-8888`;
+        phoneErr.textContent = `Precisamos de um telefone no formato: (88) 88888-8888 ou (88) 8888-8888`;
       }
     } else {
-      phoneError.textContent = '';
+      phoneErr.textContent = '';
     }
 
     // Validate Phone 2
     if (!phone2Validity.valid) {
       if (phone2Validity.patternMismatch) {
-        phone2Error.textContent = `Precisamos de um telefone no formato: (88) 98888-8888 ou (11) 8888-8888`;
+        phone2Err.textContent = `Precisamos de um telefone no formato: (88) 88888-8888 ou (88) 8888-8888`;
       }
     } else {
-      phone2Error.textContent = '';
+      phone2Err.textContent = '';
     }
 
     return false;
   }
 };
+
+// API functions
+const fetchData = async () => {
+  try {
+    const response = await fetch('https://picsum.photos/v2/list');
+    return response.json();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getURLs = async () => {
+  try {
+    const response = await fetchData();
+    return response.map(item => item.download_url);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Generators
+export const generateListItem = listContent =>
+  Object.assign(document.createElement('li'), {innerHTML: listContent});
